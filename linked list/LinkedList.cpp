f927efed9ff6188ae5169b4f79f7dcf51725723d
Geraldo Samuel <geraldosamuel916@gmail.com>
2023-01-18 20:47:19 +0100

    Implementando a função de apagar elementos

--- linked list/LinkedList.cpp
+++ linked list/LinkedList.cpp
@@ -52,6 +52,20 @@ class LinkedList{
             temp1->next = temp2->next;
             temp2->next = temp1;
         }
+        void deleteElement(int position){
+            Node * temp1 = head;
+            if(position==1){
+                head = temp1->next;
+                delete temp1;
+                return;
+            }
+            for(int i = 0; i < position-2;i++){
+                temp1 = temp1->next;
+            }
+            Node *temp2 = temp1->next;
+            temp1->next = temp2->next;
+            delete temp2;
+        }
 };
 
 int main(){

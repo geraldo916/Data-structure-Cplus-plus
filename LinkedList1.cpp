390d78457d6e057a782b75436c7731fd929b5777
Geraldo Samuel <geraldosamuel916@gmail.com>
2023-01-18 18:09:55 +0100

    Refatorando o codigo e implementar a função de inserção de elemento

--- LinkedList1.cpp
+++ LinkedList1.cpp
@@ -1,39 +1,70 @@
 #include <stdlib.h>
 #include <cstdio>
+#include <stdexcept>
 
-struct Node{
-    int data;
-    struct Node *next;
+class Node{
+    public:
+        int data;
+        Node *next;
 };
 
-struct LinkedList{
+class LinkedList{
     Node *head = NULL;
 
-    void insert(int x){
-        struct Node *temp = (Node*)malloc(sizeof(Node)) ;
-        temp->data = x;
-        temp->next = head;
-        head = temp;
-    }
-
-    void print(){
-        Node *temp;
-        while(temp != NULL){
-            printf("%d",temp->data);
-            temp = temp->next;
+    public:
+        void insert(int x){
+            Node *temp = new Node();
+            temp->data = x;
+            temp->next = head;
+            head = temp;
         }
-    }
+        int length(){
+            Node *temp = head;
+            int i = 0;
+            while(temp != NULL){
+                i = i+1;
+                temp = temp->next;
+            }
+            return i;
+        }
+        void print(){
+            Node *temp = head;
+            while(temp != NULL){
+                printf(" %d ",temp->data);
+                temp = temp->next;
+            }
+            printf("\n");
+        }
+        void insertIn(int data, int position){
+            if(position > length())  throw std::runtime_error{"The given position is over the list length"};
+            Node *temp1 = new Node();
+            temp1->data = data;
+            temp1->next = nullptr;
+            if(position == 1){
+                temp1->next = head;
+                head = temp1;
+                return;
+            }
+            Node *temp2 = head;
+            for(int i = 0; i < position-2;i++){
+                temp2 = temp2->next;
+            }
+            temp1->next = temp2->next;
+            temp2->next = temp1;
+        }
 };
 
 int main(){
     LinkedList list;
     printf("How many numbers: ");
-    int n,x,i;
+    int n,x,i,pos;
     scanf("%d",&n);
     for(i = 0;i < n;i++){
         printf("Enter the number: ");
         scanf("%d",&x);
-        list.insert(x);
+        printf("Enter the position: ");
+        scanf("%d",&pos);
+        list.insertIn(x,pos);
         list.print();
     }
 }
